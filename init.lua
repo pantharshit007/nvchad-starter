38b27f7894d96992f558f8e59de079ff34c68601
Harshit Pant <97608579+pantharshit007@users.noreply.github.com>
2026-01-03 14:46:08 +0530

    Change NvChad repository and branch in init.lua

--- init.lua
+++ init.lua
@@ -16,9 +16,9 @@ local lazy_config = require "configs.lazy"
 -- load plugins
 require("lazy").setup({
   {
-    "NvChad/NvChad",
+    "pantharshit007/NvChad",
     lazy = false,
-    branch = "v2.5",
+    branch = "self",
     import = "nvchad.plugins",
   },
 
